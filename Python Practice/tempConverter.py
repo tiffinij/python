005322726c6df947b1bfa9c9bedb992cf866d8df
tiffinij <46382532+tiffinij@users.noreply.github.com>
2021-02-27 14:41:42 -0500

    Update: tempConverter.py Finished

--- Python Practice/tempConverter.py
+++ Python Practice/tempConverter.py
@@ -8,14 +8,20 @@ converting Celsius to Fahrenheit is °F = (°C x1.8) + 32. Finally, the
 number of cool, warm and hot days should be counted and the number of
 each type of days should be displayed. You should decide on the thresholds
 for determining whether a day is cool, warm or hot.
-'''
 
+Cool - <55
+
+Warm - 55 to 74, inclusive
+
+Hot - >75 
+'''
+print('Please enter the temperatures for the days below.')
 temps = []
 i = 0
 while i < 10:
     u_input = float(input('Day {} temp: '.format(i+1)))
     y = i + 1
-    print('Day', y, 'temp:',u_input, 'ENTERED.')
+    #print('Day', y, 'temp:',u_input, 'ENTERED.')
     temps.append(u_input)
     i+=1
 
@@ -26,3 +32,19 @@ for x in range(len(temps)):
 
 
 print(temps)
+
+h = 0#hot count
+w = 0#warm count
+c = 0#cold count
+for x in temps:
+    if x >= 75:
+        h += 1
+    elif (x < 75 and x > 55):
+        w += 1
+    elif x < 55:
+        c += 1
+
+print(h, 'days were HOT')
+print(w, 'days were WARM')
+print(c, 'days were COLD')
+        
